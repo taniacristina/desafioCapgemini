97f35a635f243d8611590080ab914941eaf8f3c8
Tania Rocha <roc.ta.cris@gmail.com>
2022-02-18 17:02:32 -0300

    atualizar questao um

--- src/questao1/Escada.java
+++ src/questao1/Escada.java
@@ -1,8 +1,11 @@
+package questao1;
+
 /*
+ * Classe utilizada para alocar o método de geração de escadas com o caracter
+ * asterisco
+ * 
  * @author Tânia Rocha
  * */
-
-package questao1;
 
 public class Escada {
 
@@ -10,7 +13,8 @@ public class Escada {
 	 * Método utilizado para gerar uma escada utilizando o caracter asterisco
 	 * @param int - numero de linhas que a escada deve possuir
 	 * */
-	public void geraEscada(int n) {
+	public StringBuilder geraEscada(int n) {
+		StringBuilder escada = new StringBuilder();
 		
 		// For utilizado para gerar cada linha
 		for (int i = n; i > 0; i--) {
@@ -18,15 +22,22 @@ public class Escada {
 			// For para imprimir os espaços antes dos asteriscos
 			// o (i - 1) é utilizado para que a ultima linha não possua espaço
 			for (int j = 0; j < (i - 1) ; j++) {
-				System.out.print(" ");
+				//System.out.print(" ");
+				escada.append(" ");
 			}
 			
 			// For para imprimir o caracter asterisco
 			for (int j = i; j <= n; j++) {
-				System.out.print("*");	
+				//System.out.print("*");
+				escada.append("*");
 			}
 			
-			System.out.println(""); // For para pular linha
+			// If utilizado para pular a linha, exceto da ultima linha da escada
+			if ((i - 1) != 0) {
+				escada.append("\n");
+			}
 		}
+		
+		return escada;
 	}
 }
